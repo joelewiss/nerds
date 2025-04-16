ed67676b4d52829cebdd96d7f4728fa843129a8f
Jack Kingham <wowknead@gmail.com>
2025-04-16 00:12:02 -0600

    Fixed syntax highlighting

--- instance/app/src/components/CodeEditor.jsx
+++ instance/app/src/components/CodeEditor.jsx
@@ -7,29 +7,53 @@ import useTaskState from "../hooks/useTaskState";
 import useSavedState from "../hooks/useSavedState";
 import {isUndefined} from "../util";
 import "./CodeEditor.css";
+import { createHighlighter } from 'shiki'
+import { shikiToMonaco } from '@shikijs/monaco'
+import { Editor, useMonaco } from "@monaco-editor/react";
 
-import Editor from "@monaco-editor/react";
+
+const EDITOR_THEME = "dark-plus"
+const EDITOR_LANG = "rust"
+
+
+async function setupShikiMonaco(monaco) {
+  const highlighter = await createHighlighter({
+    themes: [EDITOR_THEME],
+    langs: [EDITOR_LANG],
+  })
+
+  // Register only the languages you plan to use
+  monaco.languages.register({ id: EDITOR_LANG })
+
+  // Inject Shiki themes and syntax rules into Monaco
+  await shikiToMonaco(highlighter, monaco)
+  monaco.editor.setTheme(EDITOR_THEME)
+}
+
 
 export default function CodeEditor(props) {
   // state that we have from props:
   // real_taskno  <int>       task number in the original ordering
   // output       <string>    the code output
   // editorRef    <ref>       Reference to the editor instance
+  const [loaded, setLoaded] = useState(false)
   const [output, setOutput] = props.output;
   const taskno = props.taskno
   const editorValue = props.editor_value;
   const setEditorValueBackend = props.set_editor_value;
   const placeholder_code = props.task.placeholder_code;
-
   const fixed = props.task.fixed;
 
-  /* Used to store if we've loaded the saved state into the monaco model */
-  const [loadedArr, setLoadedArr] = useState([]);
-  const loaded = isUndefined(loadedArr[taskno]) ? false : loadedArr[taskno];
-  const setLoaded = (v) => setLoadedArr(loadedArr => {
-    loadedArr[props.taskno] = v;
-    return loadedArr;
-  });
+  // setup additional syntax highlighting
+  let monaco = useMonaco()
+  useEffect(() => {
+    if(monaco) {
+      setupShikiMonaco(monaco).then(() => {
+        setLoaded(true)
+      })
+    }
+  }, [monaco])
+
 
   if (props.taskno == undefined) {
     console.error("real_taskno is undefined");
@@ -74,6 +98,7 @@ export default function CodeEditor(props) {
   });
 
 
+  if (!loaded) return <div>Loading editor...</div>
   return fixed ? (
     <></>
   ) : (
@@ -92,11 +117,12 @@ export default function CodeEditor(props) {
             }} />
       </div>
       <Editor
-        language="rust"
+        language='rust'
+        defaultLanguage='rust'
         options={{domReadOnly: false, readOnly: false}}
         path={`task${props.taskno}`}
         defaultValue={editorValue}
-        theme="vs-dark"
+        theme={EDITOR_THEME}
         onMount={handleEditorDidMount}
         onChange={handleEditorDidChange}
         wrapperProps={{"style":{"flex":"2 1 400px", "minHeight":"200px", "padding": "0.5em"}}}
